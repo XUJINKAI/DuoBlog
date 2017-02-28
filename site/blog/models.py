9b2eb37f1969e90f2f47072593f923e6744cdcb3
XJK-S5VS <xujinkai@gmail.com>
2017-03-01 03:34:42 +0800

    美化note:index。 未完成：BUS如何处理/保存多选posts

--- site/blog/models.py
+++ site/blog/models.py
@@ -76,7 +76,7 @@ class Blog(models.Model):
 	@classmethod
 	def create_new(cls):
 		domain = get_random_id(8)
-		name = 'MultBlog+' + get_random_id(3)
+		name = 'MultBlog_' + get_random_id(3)
 		blog = Blog.objects.create(domain=domain, name=name, navs=DEFAULT_NAVS())
 		return blog
 
